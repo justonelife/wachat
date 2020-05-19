b195953924f8ea9a0e3fdd6a22dba6992b188827
justonelife <ngnhut07051998@outlook.com>
2020-05-19 21:25:35 +0700

    add Avatar & ContactBox[!] component

--- app_compiled.js
+++ app_compiled.js
@@ -6,8 +6,76 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var App = function (_React$Component) {
-	_inherits(App, _React$Component);
+var Avatar = function (_React$Component) {
+	_inherits(Avatar, _React$Component);
+
+	function Avatar() {
+		_classCallCheck(this, Avatar);
+
+		return _possibleConstructorReturn(this, (Avatar.__proto__ || Object.getPrototypeOf(Avatar)).apply(this, arguments));
+	}
+
+	_createClass(Avatar, [{
+		key: 'render',
+		value: function render() {
+			var size = this.props.size + 'px';
+			var no_pic = React.createElement('i', { className: 'fas fa-user-astronaut avatar-default' });
+			return React.createElement(
+				'div',
+				{ className: 'avatar avatar--default',
+					style: { width: size,
+						height: size } },
+				no_pic
+			);
+		}
+	}]);
+
+	return Avatar;
+}(React.Component);
+
+var ContactBox = function (_React$Component2) {
+	_inherits(ContactBox, _React$Component2);
+
+	function ContactBox() {
+		_classCallCheck(this, ContactBox);
+
+		return _possibleConstructorReturn(this, (ContactBox.__proto__ || Object.getPrototypeOf(ContactBox)).apply(this, arguments));
+	}
+
+	_createClass(ContactBox, [{
+		key: 'render',
+		value: function render() {
+			var type = this.props.type;
+			var bg = this.props.theme;
+
+			var box = type === 'blank' ? React.createElement(
+				'div',
+				{ className: 'contact-box',
+					style: { backgroundColor: bg } },
+				React.createElement(
+					'button',
+					{ className: 'contact-box__addBtn' },
+					React.createElement('i', { className: 'fas fa-user-plus' })
+				)
+			) : React.createElement(
+				'h1',
+				null,
+				'wrong'
+			);
+
+			return React.createElement(
+				React.Fragment,
+				null,
+				box
+			);
+		}
+	}]);
+
+	return ContactBox;
+}(React.Component);
+
+var App = function (_React$Component3) {
+	_inherits(App, _React$Component3);
 
 	function App() {
 		_classCallCheck(this, App);
@@ -19,9 +87,11 @@ var App = function (_React$Component) {
 		key: 'render',
 		value: function render() {
 			return React.createElement(
-				'h1',
+				React.Fragment,
 				null,
-				'Hello'
+				React.createElement(Avatar, { size: '60' }),
+				' ',
+				React.createElement(ContactBox, { theme: '#5ED2D2', type: 'blank' })
 			);
 		}
 	}]);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,52 @@
+class Avatar extends React.Component {
+	render() {
+		var size = this.props.size + 'px';
+		var no_pic = <i className="fas fa-user-astronaut avatar-default"></i>;
+		return (
+			<div className='avatar avatar--default'
+				 style={{width: size,
+						 height: size}}>
+
+				{no_pic}
+			</div>
+		);
+	}
+}
+
+class ContactBox extends React.Component {
+	render() {
+		var type = this.props.type;
+		var bg = this.props.theme;
+
+		var box = type === 'blank' ?
+			(
+				<div className='contact-box' 
+					 style={{backgroundColor: bg}}>
+
+					<button className='contact-box__addBtn'>
+						<i className="fas fa-user-plus"></i>
+					</button>
+				</div>
+			) :
+			( 
+				<h1>wrong</h1>
+			);
+
+		return (
+			<React.Fragment>
+				{box}
+			</React.Fragment>
+		);
+	}
+}
+
 class App extends React.Component {
 	render() {
 		return (
-			<h1>Hello</h1>
+			<React.Fragment>
+				<Avatar size='60' /> {/*add pic property*/}
+				<ContactBox theme='#5ED2D2' type='blank' />
+			</React.Fragment>
 		);
 	}
 }
